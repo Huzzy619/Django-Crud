2bcb9f4bb22ae959822e886c2f4e7fdc5444b048
Huzzy619 <blazingkrane@gmail.com>
2022-06-24 23:15:42 +0100

    Allow default

--- I4G025323ITA/settings.py
+++ I4G025323ITA/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-t9fn84hu$55k4rbrhhq1s5(13a9uj*$qe7#zkn6$k69!a4y0ky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,6 @@ from .models import Post
 
 class PostListView (ListView):
     model = Post
-    template_name = 'blog/post_list.html'
     
 
 
@@ -15,23 +14,19 @@ class PostCreateView (CreateView):
     model = Post
     fields = '__all__'
     success_url = reverse_lazy('blog:all')
-    template_name = 'blog/post_form.html'
 
 
 class PostDetailView (DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
 
 
 class PostUpdateView (UpdateView):
     model = Post
     fields = '__all__'
     success_url = reverse_lazy('blog:all')
-    template_name = 'blog/post_form.html'
 
 
 class PostDeleteView (DeleteView):
     model = Post
     fields = '__all__'
     success_url = reverse_lazy('blog:all')
-    template_name = 'blog/post_confirm_delete.html'
